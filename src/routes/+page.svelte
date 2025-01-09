5a1f7a397650663c52558b62147b83c3ff4a7889
thortek <ta.anderson@gmail.com>
2025-01-08 17:04:28 -0700

    Added a Skeleton App Bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1 class="text-4xl text-center m-6">Welcome to Thor's Full_Stack SvelteKit App!</h1>
+<script lang="ts">
+	import { AppBar } from '@skeletonlabs/skeleton-svelte';
+	import ArrowLeft from 'lucide-svelte/icons/arrow-left';
+	import Paperclip from 'lucide-svelte/icons/paperclip';
+	import Calendar from 'lucide-svelte/icons/calendar';
+	import CircleUser from 'lucide-svelte/icons/circle-user';
+</script>
+
+<AppBar>
+	{#snippet lead()}
+		<ArrowLeft size={24} />
+	{/snippet}
+	{#snippet trail()}
+		<Paperclip size={20} />
+		<Calendar size={20} />
+		<CircleUser size={20} />
+	{/snippet}
+	{#snippet children()}
+		<p class="text-blue-500">Center Align</p>
+	{/snippet}
+	<!--     {#snippet headline()}
+      <p class="bg-slate-700 text-indigo-200 text-center">Headline</p>
+    {/snippet} -->
+</AppBar>
+
+<h1 class="m-6 text-center text-4xl">Welcome to Thor's Full_Stack SvelteKit App!</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
