140cc1fd52c6312c3b99397f98a2508a80d2505c
thortek <ta.anderson@gmail.com>
2025-01-15 16:42:15 -0700

    Basic chat user route connects with chat api

---
+++ src/routes/api/chat/+server.ts
@@ -0,0 +1,28 @@
+import OpenAI from 'openai';
+
+// Create a new OpenAI instance to connect with your OpenAI API key
+//const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY})
+
+const openai = new OpenAI({
+    baseURL: 'http://localhost:11434/v1',
+    apiKey: 'ollama', // required but unused
+  })
+
+//export type MessageBody = { chats: { role: 'user' | 'assistant'; content: string }[]}
+
+export const POST = async ({ request }) => {
+    //const body: MessageBody = await request.json()
+  
+  const completion = await openai.chat.completions.create({
+    model: 'llama3.2',
+    messages: [
+        { role: 'system', content: 'You are a helpful assistant.' },
+        { role: 'assistant', content: 'What can I help you with today?' },
+        { role: 'user', content: 'What can you tell me about Svelte?' }
+    ],
+  })
+  
+  console.log(completion.choices[0].message.content)
+
+  return new Response(JSON.stringify({ message: completion.choices[0].message.content }))
+}
---
+++ src/routes/chat/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	async function handleSubmit(event: Event) {
+		event?.preventDefault();
+        try {
+            const response = new Request('/api/chat', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    message: (event?.target as HTMLFormElement).querySelector('input')?.value,
+                })
+            })
+            const answer = await fetch(response);
+            console.log(answer);
+        } catch (error) {
+            console.error(error);
+        }
+	}
+</script>
+
+<main>
+	<h1>Chat</h1>
+	<form onsubmit={handleSubmit}>
+		<input required type="text" placeholder="Type your message" />
+		<button type="submit">Send</button>
+	</form>
+</main>
