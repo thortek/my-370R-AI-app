2a97a77708af83914b51cfdde06d319e0ff097cd
thortek <ta.anderson@gmail.com>
2025-02-12 16:50:18 -0700

    Setup script to define text embedding schema

---
+++ src/scripts/defineEmbeddedFile.ts
@@ -0,0 +1,85 @@
+import weaviate, { dataType, type WeaviateClient, type Collection } from 'weaviate-client'
+
+let client: WeaviateClient
+
+// the following function is custom for Thor's machine only!!
+async function connectToWeaviate() : Promise<WeaviateClient> {
+    const clientPromise = weaviate.connectToCustom({
+		httpHost: 'localhost',
+		httpPort: 8084,
+		grpcHost: 'localhost',
+		grpcPort: 50054
+	});
+	return clientPromise;
+}
+
+const embeddedFileSchema = {
+    name: 'Chunks',
+    description: 'Relevant chunks of text from selected PDF files',
+    vectorizers: weaviate.configure.vectorizer.text2VecOllama({
+        // Configure the Ollama embedding integration
+        model: 'nomic-embed-text',
+        apiEndpoint: 'http://host.docker.internal:11434'
+    }),
+    generative: weaviate.configure.generative.ollama({
+        model: 'llama3.2',
+        apiEndpoint: 'http://host.docker.internal:11434'
+    }),
+    properties: [
+        {
+            name: 'chunk_text',
+            dataType: dataType.TEXT
+        },
+        {
+            name: 'file_name',
+            dataType: dataType.TEXT
+        },
+        {
+            name: 'metadata',
+            dataType: dataType.OBJECT,
+            properties: [
+                {
+                    name: 'totalPages',
+                    dataType: dataType.INT
+                },
+                {
+                    name: 'pageNumberLocation',
+                    dataType: dataType.INT
+                },
+                {
+                    name: 'chunkIndex',
+                    dataType: dataType.INT
+                }
+            ]
+        }
+    ]
+}
+
+async function addCollection() {
+    try {
+        await client.collections.create(embeddedFileSchema)
+        console.log('Added the Chunks collection')
+    } catch (err) {
+        console.error('Failed to add the Chunks collection')
+    }
+}
+
+async function run() {
+    const startTime: Date = new Date()
+    console.log('Starting user embedded file schema creation...')
+
+    client = await connectToWeaviate()
+    await addCollection()
+    //await client.collections.delete('Chunks')
+
+    const endTime: Date = new Date()
+    const elapsedTime: number = endTime.getTime() - startTime.getTime();
+	const elapsedTimeSeconds: number = elapsedTime / 1000;
+	const elapsedTimeMinutes: number = elapsedTimeSeconds / 60;
+
+	console.log(
+		`Total running time: ${elapsedTimeSeconds} seconds or ${elapsedTimeMinutes} minutes.`
+	);
+}
+
+await run()
